7d6d6c3b9d794ea8a64f1a1906277b5b86783809
b0rke-mborina <mborina@net.hr>
2023-05-16 12:23:05 +0200

    work on dish and ingredient views

--- app/src/main/java/android/meal_chooser/DishActivity.java
+++ app/src/main/java/android/meal_chooser/DishActivity.java
@@ -2,14 +2,25 @@ package android.meal_chooser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+
 import java.util.Objects;
 
 public class DishActivity extends AppCompatActivity {
+    private LinearLayout mIngredientListView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dish);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        mIngredientListView = findViewById(R.id.ingredient_list_view);
+        // LinearLayout item1 = new LinearLayout(getApplicationContext()); // R.layout.dish_ingredient_item
+        // item1.setLayout();
+        // LinearLayout item1 = LayoutInflater.from(getApplicationContext()).inflate(R.layout.dish_ingredient_item);
+        // mIngredientListView.addView(item1);
     }
 
     /**
